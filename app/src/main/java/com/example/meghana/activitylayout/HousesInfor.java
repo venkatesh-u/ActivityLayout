9a0012e6668571499357f83fa43aaa83680e4370
Venkatesh <venkatesh@Venkatesh.(none)>
2016-05-30 14:32:43 +0530

    my second commit

---
+++ app/src/main/java/com/example/meghana/activitylayout/HousesInfor.java
@@ -0,0 +1,24 @@
+package com.example.meghana.activitylayout;
+
+/**
+ * Created by meghana on 29/2/16.
+ */
+public class HousesInfor {
+
+
+    private String name;
+    private boolean type;
+
+    public HousesInfor(String name, boolean type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public boolean isType() {
+        return type;
+    }
+
+}
